bbadaf240dafecc66a1ae9d7618fe1707d98fb9e
N-Dave-08 <cdave.dev@gmail.com>
2024-07-15 21:52:58 +0800

    updated to cleaner code

--- reactCounterProgram/src/Counter.jsx
+++ reactCounterProgram/src/Counter.jsx
@@ -3,26 +3,14 @@ import styles from "./Buttons/Buttons.module.css"
 
 function Counter() {
 
-    let [num, setNum] = useState(0);
-
-    const increaseValue = () => {
-        setNum(num + 1);
-    }
-
-    const resetValue = () => {
-        setNum(num = 0);
-    }
-
-    const decreaseValue = () => {
-        setNum(num - 1);
-    }
+    const [num, setNum] = useState(0);
 
     return (
         <>
             <p className="number">{num}</p>
-            <button className={styles.counterButton} onClick={decreaseValue}>minus</button>
-            <button className={styles.counterButton} onClick={resetValue}>reset</button>
-            <button className={styles.counterButton} onClick={increaseValue}>plus</button>
+            <button className={styles.counterButton} onClick={() => setNum((num) => num -1)}>minus</button>
+            <button className={styles.counterButton} onClick={() => setNum((num) => num = 0)}>reset</button>
+            <button className={styles.counterButton} onClick={() => setNum((num) => num + 1)}>plus</button>
         </>
     );
 }
