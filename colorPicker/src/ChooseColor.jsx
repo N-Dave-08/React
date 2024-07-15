cf905212c0d424cc50daac17f8ae777803f5dfba
N-Dave-08 <cdave.dev@gmail.com>
2024-07-15 23:55:50 +0800

    add interactive box shadow and radius

--- colorPicker/src/ChooseColor.jsx
+++ colorPicker/src/ChooseColor.jsx
@@ -4,20 +4,50 @@ import styles from "./Box/Box.module.css"
 function ChooseColor() {
 
     const [color, setColor] = useState("#757575");
-    const [radius, setRadius] = useState("10");
+    const [radius, setRadius] = useState("10px");
+    const [shadowColor, setshadowColor] = useState("#757575");
+    const [shadowHorizontal, setshadowHorizontal] = useState("0px");
+    const [shadowVertical, setshadowVertical] = useState("0px");
+    const [shadowBlur, setshadowBlur] = useState("30px");
 
     return (
         <>
             <h1>Color Picker</h1>
-            <div className={styles.box} style={{backgroundColor: color, borderRadius: radius}}>
-                
+            <div
+                className={styles.box}
+                style={{
+                    backgroundColor: color,
+                    borderRadius: radius,
+                    boxShadow: `${shadowHorizontal} ${shadowVertical} ${shadowBlur} ${shadowColor}`
+                }}>
             </div>
-            <br />
-            <p>Selected Color: {color}</p>
-            <input type="color" value={color} onChange={(event) => setColor(event.target.value)}/>
-            <br />
-            <input type="range" value={parseInt(radius)} onChange={(event) => setRadius(`${event.target.value}px`)}/>
-            <p>Radius: {`${radius}`}</p>
+
+            <div className={styles.main}>
+                <div className={styles.indicator}>
+                    <p>Selected Color: {color}</p>
+                    <input type="color" value={color} onChange={(event) => setColor(event.target.value)} />
+                </div>
+                <div className={styles.indicator}>
+                    <p>Radius: {`${radius}`}</p>
+                    <input type="range" value={parseInt(radius)} onChange={(event) => setRadius(`${event.target.value}px`)} />
+                </div>
+                <div className={styles.indicator}>
+                    <p>Shadow Color: {color}</p>
+                    <input type="color" value={shadowColor} onChange={(event) => setshadowColor(event.target.value)} />
+                </div>
+                <div className={styles.indicator}>
+                    <p>Horizontal: {`${shadowHorizontal}`}</p>
+                    <input type="range" value={parseInt(shadowHorizontal)} onChange={(event) => setshadowHorizontal(`${event.target.value}px`)} />
+                </div>
+                <div className={styles.indicator}>
+                    <p>Vertical: {`${shadowVertical}`}</p>
+                    <input type="range" value={parseInt(shadowVertical)} onChange={(event) => setshadowVertical(`${event.target.value}px`)} />
+                </div>
+                <div className={styles.indicator}>
+                    <p>Blur: {`${shadowBlur}`}</p>
+                    <input type="range" value={parseInt(shadowBlur)} onChange={(event) => setshadowBlur(`${event.target.value}px`)} />
+                </div>
+            </div>
         </>
     );
 }
